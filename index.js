29d13e72419809a348566f12c87f629bf039b38a
Adrian Lee <adrianlee44@gmail.com>
2014-04-15 02:04:23 -0700

    Converted ical2json to full javascript
    Closes #1

--- index.js
+++ index.js
@@ -1,82 +1,88 @@
-(function() {
-  var NEW_LINE, convert, cwd, fs, path, run;
+var NEW_LINE = /\r\n|\n|\r/,
+    path = require("path"),
+    fs = require("fs"),
+    cwd = process.cwd(),
+    convert, run, readFile;
 
-  fs = require("fs");
+convert = function(source) {
+  var currentKey = "",
+      currentObj,
+      currentValue = "",
+      line,
+      objectNames = [],
+      output = {},
+      parentObj = {},
+      i,
+      linesLength,
+      lines = source.split(NEW_LINE),
+      lineArray;
 
-  path = require("path");
+  currentObj = output;
 
-  cwd = process.cwd();
+  for (i = 0, linesLength = lines.length; i < linesLength; i++) {
+    line = lines[i];
+    if (line.charAt(0) === " ") {
+      currentObj[currentKey] += line;
 
-  NEW_LINE = /\n\r?/;
+    } else {
+      lineArray = line.split(":");
+      currentKey = lineArray[0];
+      currentValue = lineArray[1];
 
-  convert = function(source) {
-    var currentKey, currentObj, currentValue, line, objectNames, output, parentObj, _i, _len, _ref, _ref1;
-    output = {};
-    currentObj = output;
-    parentObj = {};
-    currentKey = "";
-    currentValue = "";
-    objectNames = [];
-    _ref = source.split(NEW_LINE);
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      line = _ref[_i];
-      if (line.charAt(0) === " ") {
-        currentObj[currentKey] += line;
-      } else {
-        _ref1 = line.split(":"), currentKey = _ref1[0], currentValue = _ref1[1];
-        switch (currentKey) {
-          case "BEGIN":
-            parentObj = currentObj;
-            if (parentObj[currentValue] == null) {
-              parentObj[currentValue] = [];
-            }
-            currentObj = {};
-            parentObj[currentValue].push(currentObj);
-            break;
-          case "END":
-            currentObj = parentObj;
-            break;
-          default:
-            currentObj[currentKey] = currentValue;
-        }
+      switch (currentKey) {
+        case "BEGIN":
+          parentObj = currentObj;
+          if (parentObj[currentValue] == null) {
+            parentObj[currentValue] = [];
+          }
+          currentObj = {};
+          parentObj[currentValue].push(currentObj);
+          break;
+        case "END":
+          currentObj = parentObj;
+          break;
+        default:
+          currentObj[currentKey] = currentValue;
       }
     }
-    return output;
-  };
+  }
+  return output;
+};
 
-  run = function(options) {
-    var ext, file, filePath, files, stat, _i, _len, _results;
-    files = options.args || [];
-    _results = [];
-    for (_i = 0, _len = files.length; _i < _len; _i++) {
-      file = files[_i];
-      filePath = path.resolve(cwd, file);
-      if (!fs.existsSync(filePath)) {
-        continue;
-      }
-      stat = fs.statSync(filePath);
-      ext = path.extname(filePath);
-      if (!(stat.isFile() && ext === ".ics")) {
-        continue;
-      }
-      _results.push(fs.readFile(filePath, function(error, buffer) {
-        var basename, dirname, output, writePath;
-        if (error != null) {
-          throw new Error(error);
-        }
-        output = convert(buffer.toString());
-        basename = path.basename(filePath, ext);
-        dirname = path.dirname(filePath);
-        writePath = path.join(dirname, basename) + ".json";
-        return fs.writeFile(writePath, JSON.stringify(output, null, "  "));
-      }));
+run = function(options) {
+  var ext, file, filePath, files, stat, i, length;
+  files = options.args || [];
+
+  for (i = 0, length = files.length; i < length; i++) {
+    file = files[i];
+    filePath = path.resolve(cwd, file);
+
+    if (!fs.existsSync(filePath)) {
+      continue;
     }
-    return _results;
-  };
 
-  module.exports = {
-    run: run,
-    convert: convert
-  };
+    stat = fs.statSync(filePath);
+    ext = path.extname(filePath);
 
-}).call(this);
+    if (!(stat.isFile() && ext === ".ics")) {
+      continue;
+    }
+
+    fs.readFile(filePath, function(error, buffer) {
+      var basename, dirname, output, writePath;
+      if (error != null) {
+        throw new Error(error);
+      }
+      output = convert(buffer.toString());
+      basename = path.basename(filePath, ext);
+      dirname = path.dirname(filePath);
+      writePath = path.join(dirname, basename) + ".json";
+      return fs.writeFile(writePath, JSON.stringify(output, null, "  "));
+    });
+  }
+};
+
+module.exports = {
+  run: run,
+  convert: convert
+};
