a53b23699200352c924ba37300b3ed6be4cdf564
Adrian Lee <adrianlee44@gmail.com>
2016-12-18 22:08:17 -0800

    Remove watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,19 +10,9 @@ module.exports = function(grunt) {
       },
       src: 'index.js',
       test: 'test.js'
-    },
-    watch: {
-      src: {
-        files: 'index.js',
-        tasks: ['jshint', 'nodeunit']
-      },
-      test: {
-        files: 'test.js',
-        tasks: ['jshint', 'nodeunit']
-      }
     }
   });
-  
+
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('default', ['jshint', 'nodeunit']);
