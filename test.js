ccc13f81266d259fec3ccf27596840f4c3b0967b
Adrian Lee <adrianlee44@gmail.com>
2016-12-19 01:15:06 -0800

    Add tests for reading and writing files

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var ical2json = require("./index"),
+    fs = require("fs"),
     eventString = "BEGIN:VEVENT\nDTSTART;VALUE=DATE:20130101\nDTEND;VALUE=DATE:20130102\nDTSTAMP:20111213T124028Z\nUID:9d6fa48343f70300fe3109efe@calendarlabs.com\nCREATED:20111213T123901Z\nDESCRIPTION:Visit http://calendarlabs.com/holidays/us/new-years-day.php to kn\n ow more about New Year's Day. Like us on Facebook: http://fb.com/calendarlabs to get updates.\nLAST-MODIFIED:20111213T123901Z\nLOCATION:\nSEQUENCE:0\nSTATUS:CONFIRMED\nSUMMARY:New Year's Day\nTRANSP:TRANSPARENT\nEND:VEVENT";
 
 exports.convert = {
@@ -83,3 +84,23 @@ exports["convert multi-child w/o BEGIN-END"] =  {
     test.done();
   }
 };
+
+exports.run = {
+  setUp: function (done) {
+    fs.writeFileSync("test.ics", eventString);
+    fs.writeFileSync("wrongExt.data", eventString);
+    done();
+  },
+
+  "read .ics and write .json": function (test) {
+    ical2json.run({
+      args: ["doesNotExist.ics", "wrongExt.data", "test.ics"]
+    })
+    .then(function () {
+      test.ok(!fs.existsSync("doesNotExist.json"));
+      test.ok(!fs.existsSync("wrongExt.json"));
+      test.ok(fs.existsSync("test.json"));
+      test.done();
+    });
+  }
+}
